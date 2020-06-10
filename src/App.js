1d4a96bed113863a97c27ff7392f3e011838209f
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-06-10 12:54:00 -0400

    update folders

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Politically Squared</h1>
+      <h4>Make Critical Thinking Great Again!</h4>
+      <p>Established 2020 - Coming Soon!</p>
+    </div>
+  );
+}
+
+export default App;
